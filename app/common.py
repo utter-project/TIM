082c1d49f7c4274f831d78afc9398ec55505a277
Jos Rozen <jos.rozen@naverlabs.com>
2024-03-06 15:52:17 +0100

    extended to local models via openai's API

--- app/common.py
+++ app/common.py
@@ -1,14 +1,3 @@
-PROVIDER = "replicate"
-REPLICATE_MODEL = "meta/llama-2-7b-chat:52551facc68363358effaacb0a52b5351843e2d3bf14f58aff5f0b82d756078c"
-
-CONTEXT_SIZE = 4096
-MAX_TOKENS = 512
-
-MODELS = {
-    "instruct": "text-davinci-003",
-    "chat": "gpt-3.5-turbo"
-}
-
 import streamlit as st
 
 import json, os
@@ -17,61 +6,52 @@ from datetime import datetime
 import openai, tiktoken
 import replicate
 
-openai.api_key = os.environ["OPENAI_API_KEY"]
+CONTEXT_SIZE = 4096
+MAX_TOKENS = 512
+
+PARAMETERS = {
+    "replicate": {"model": "meta/llama-2-7b-chat:52551facc68363358effaacb0a52b5351843e2d3bf14f58aff5f0b82d756078c", "api_key": "", "base_url": ""},
+    "LMStudio": {"model": "local-model", "api_key": "not-needed", "base_url": "http://localhost:1234/v1"},
+    "openai": {"model": "gpt-3.5-turbo",  "api_key": os.environ["OPENAI_API_KEY"], "base_url": "http://api.openai.com/v1"}
+}
 
 default = {"max_tokens": MAX_TOKENS,
     "temperature": 1,
     "top_p": 0.92,
 }
 
-instruct_default = default.copy()
-instruct_default["engine"] = MODELS["instruct"]
+provider = os.environ["llm_provider"]
+parameters = default.copy()
+parameters["model"] = PARAMETERS[provider]["model"]
 
-instruct_gpt = lambda p: openai.Completion.create(**{**instruct_default, **p}).choices[0].text
-
-chat_default = default.copy()
-chat_default["model"] = MODELS["chat"]
-
-chat_gpt = lambda ms: openai.ChatCompletion.create(**{**chat_default, **ms}).choices[0].message.content
-
-dialog = lambda us: '\n'.join([{"user": st.session_state.human, "assistant": st.session_state.assistant}[r] + f": {u}" for (r, u) in us])
-
-get_stamp = lambda c: datetime.now().strftime(f"%Y%m%d{c}%H%M%S")
-
-def openai_utter(utterance, system, model_family="chat"):
+def openai_utter(utterance, system):
     response, error = '', ''
     try:
-        if "chat" == model_family:
-            messages = [{"role": "system", "content": system}]
-            for r, u in st.session_state.utterances:
-                messages.append({"role": r, "content": u})
-            messages.append({"role": "user", "content": utterance})
-            response = chat_gpt({"messages": messages}).rstrip()
+        messages = [{"role": "system", "content": system}]
+        for r, u in st.session_state.utterances:
+            messages.append({"role": r, "content": u})
+        messages.append({"role": "user", "content": utterance})
+        response = chat({"messages": messages}).rstrip()
 
-        else:
-            context = '\n'.join([system, '\n', dialog(st.session_state.utterances)])
-            prompt = f"{context}\n{st.session_state.human}: {utterance}\n{st.session_state.assistant}:"
-            response = instruct_gpt({"prompt": prompt, "stop": f"{st.session_state.human}:"})
-
-    except openai.error.APIError as e:
+    except openai.APIError as e:
         error = (f"OpenAI API returned an API Error: {e}")
         pass
-    except openai.error.APIConnectionError as e:
+    except openai.APIConnectionError as e:
         error = f"Failed to connect to OpenAI API: {e}"
         pass
-    except openai.error.RateLimitError as e:
+    except openai.RateLimitError as e:
         error = f"OpenAI API request exceeded rate limit: {e}"
         pass
-    except openai.error.Timeout as e:
+    except openai.Timeout as e:
         error = f"OpenAI API request timed out: {e}"
         pass
-    except openai.error.InvalidRequestError as e:
+    except openai.InvalidRequestError as e:
         error = f"Invalid request to OpenAI API: {e}"
         pass
-    except openai.error.AuthenticationError as e:
+    except openai.AuthenticationError as e:
         error = f"Authentication error with OpenAI API: {e}"
         pass
-    except openai.error.ServiceUnavailableError as e:
+    except openai.ServiceUnavailableError as e:
         error = f"OpenAI API service unavailable: {e}"
         pass
     return response.rstrip(), error
@@ -83,7 +63,7 @@ def replicate_utter(utterance, system, _):
     messages.append(userify(utterance))
 
     output = replicate.run(
-        REPLICATE_MODEL,
+        PARAMETERS[provider]["model"],
         input={
             "prompt": '\n'.join(messages),
             "system": system
@@ -100,12 +80,20 @@ def replicate_utter(utterance, system, _):
 
     return response.rstrip(), error
 
+if provider in ["openai", "LMStudio"]:
+    client = openai.Client(base_url=PARAMETERS[provider]["base_url"], api_key=PARAMETERS[provider]["api_key"])
+    chat = lambda ms: client.chat.completions.create(**{**parameters, **ms}).choices[0].message.content
+    utter_function = openai_utter
+else:
+    utter_function = replicate_utter
 
-def utter(utterance, model_family="chat"):
+dialog = lambda us: '\n'.join([{"user": st.session_state.human, "assistant": st.session_state.assistant}[r] + f": {u}" for (r, u) in us])
+
+def utter(utterance, function=utter_function):
+    print(utterance)
     system = '\n'.join([st.session_state.background, st.session_state.setup])
 
-    f = openai_utter if "openai" == PROVIDER else replicate_utter
-    return f(utterance, system, model_family)
+    return function(utterance, system)
 
 boldify = lambda l: ("**" + l.replace(":", "**:", 1)) if l.startswith(st.session_state.human) or l.startswith(st.session_state.assistant) else l
 
@@ -125,6 +113,8 @@ def command(cmd, password=None):
     else:
         st.error(":rotating_light: unknown command!")
 
+get_stamp = lambda c: datetime.now().strftime(f"%Y%m%d{c}%H%M%S")
+
 def save():
     conversation = {
         "scenario": st.session_state.scenario,
@@ -133,8 +123,7 @@ def save():
         "human": st.session_state.human,
         "assistant": st.session_state.assistant,
         "utterances": st.session_state.utterances,
-        "model_family": st.session_state.model_family,
-        "model": MODELS[st.session_state.model_family],
+        "model": PARAMETERS[provider]["model"],
     }
     if "help" in st.session_state:
         conversation["help"] = st.session_state.help
@@ -148,7 +137,7 @@ def save():
     with open(f"save/{st.session_state.scenario.lower().replace(' ', '_')}.{user_id_stamp}.json", 'w') as json_file:
         json.dump(conversation, json_file)
     
-    enc = tiktoken.encoding_for_model(MODELS[st.session_state.model_family])
+    enc = tiktoken.encoding_for_model(PARAMETERS["openai"]["model"]) # IMPROVE THIS
 
     n_tokens = st.session_state.n_tokens\
         + sum([len(enc.encode(u)) for (_, u) in st.session_state.utterances])
@@ -174,15 +163,11 @@ def upload(name="default"):
         st.session_state.scenario = conversation["scenario"]
         if "help" in conversation:
             st.session_state.help = conversation["help"]
-        if "model_family" in conversation:
-            st.session_state.model_family = conversation["model_family"]
-        else:
-            st.session_state.model_family = "instruct"
         st.session_state.human = conversation["human"] if "human" in conversation else "Human"
         st.session_state.assistant = conversation["assistant"] if "assistant" in conversation else "Agent"
         st.session_state.utterances = conversation["utterances"] if "utterances" in conversation else list()
 
-        enc = tiktoken.encoding_for_model(MODELS[st.session_state.model_family])
+        enc = tiktoken.encoding_for_model(PARAMETERS["openai"]["model"]) # IMPROVE THIS
 
         st.session_state.n_tokens = len(enc.encode(st.session_state.background))
         st.session_state.n_tokens += len(enc.encode(st.session_state.setup))
--- app/home.py
+++ app/home.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 from app.common import dialog, boldify, go_back, save, upload, utter, get_stamp
 
+print("top")
 
 help = """`speak` sends your utterance to the agent,  
 `retry` requests an alternate response from the agent,  
@@ -11,7 +12,7 @@ Note that the conversation is saved to a log file after each turn of utterance a
 """
 
 if "scenario" not in st.session_state:
-    params = st.experimental_get_query_params()
+    params = st.query_params
     if "scenario" in params:
         upload(params["scenario"][0])
     else:
@@ -26,6 +27,13 @@ with tab1:
     st.image("assets/tim.jpeg")
 
 with tab2:
+    # this is quite hacky...
+    if "FormSubmitter:dialog-Retry" in st.session_state\
+    or "FormSubmitter:dialog-Revert" in st.session_state:
+        if st.session_state["FormSubmitter:dialog-Retry"]\
+        or st.session_state["FormSubmitter:dialog-Revert"]:
+            go_back()
+
     st.subheader(st.session_state.scenario)
 
     if "utterances" not in st.session_state:
@@ -35,43 +43,32 @@ with tab2:
         utterance = st.text_input(st.session_state.human, key="utterance")
         c1, c2, c3 = st.columns(3)
         with c1:
-            submit = st.form_submit_button(label="Speak", disabled=not st.session_state.speakable,)
+            submit = st.form_submit_button(label="Speak", disabled=not st.session_state.speakable)
         with c2:
-            retry = st.form_submit_button(label="Retry", disabled='' == utterance, on_click=go_back)
-        with c3:
-            revert = st.form_submit_button(label="Revert", disabled='' == utterance, on_click=go_back)
-
-    if submit or retry:
-        if '' != utterance:
-            answer, error = utter(utterance)
-            warning = "sorry but the agent couldn't answer. You might want to try again..."
-            if '' != error:
-                print(get_stamp(':'), error)
-                st.warning(warning, icon="😵‍💫")
-            elif '' != error:
-                print(get_stamp(':'), "no answer: was Streamlit's stop pressed?")
-                st.warning(warning, icon="😵‍💫")
+            if utterance:
+                retry = st.form_submit_button(label="Retry")
             else:
-                st.session_state.utterances.append(("user", utterance))
-                st.session_state.utterances.append(("assistant", answer))
+                retry = ''
+        with c3:
+            if utterance:
+                revert = st.form_submit_button(label="Revert")
+            else:
+                revert = ''
 
-    code_mode = False
-    code = list()
+        if submit or retry:
+            if '' != utterance:
+                answer, error = utter(utterance)
+                warning = "sorry but the agent couldn't answer. You might want to try again..."
+                if '' != error:
+                    print(get_stamp(':'), error)
+                    st.warning(warning, icon="😵‍💫")
+                else:
+                    st.session_state.utterances.append(("user", utterance))
+                    st.session_state.utterances.append(("assistant", answer))
+
     for l in dialog(st.session_state.utterances[::-1]).split('\n'):
-        if l.startswith("#"):
-            code_mode = True
-        if l.startswith(st.session_state.human)\
-        or l.startswith(st.session_state.assistant):
-            if code:
-                st.code('\n'.join(code), language='python')
-            code_mode = False
-            code = list()
-        if code_mode:
-            code.append(l)
-        else:
-            st.write(boldify(l))
-    if code:
-        st.code('\n'.join(code), language='python')
+        st.write(boldify(l))
+
     save()
 
 with tab3:

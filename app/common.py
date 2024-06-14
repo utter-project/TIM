886fc82ae26f681a9534f59c3b5d2638f3b023b0
laurent-besacier <lbesacie@berkners-MacBook-Pro.local>
2024-06-14 17:05:57 +0200

    change the code and add a specific scenario 'meeting'

--- app/common.py
+++ app/common.py
@@ -5,14 +5,20 @@ import langchain_core
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_community.chat_models import ChatOllama
+
 
 
 MODELS = {
-    "LMStudio": {"model": "local-model", "api_key": "not-needed", "base_url": "http://localhost:1234/v1"},
+    #"local": {"model": "mistral-ins-7b-q4", "api_key": "not-needed", "base_url": "http://127.0.0.1:1337/"},
+    "local": {"model": "gemma-2b", "api_key": "not-needed", "base_url": "http://127.0.0.1:1337/"},
+    #"nle": {"model": "phi3:instruct", "api_key": "not-needed", "base_url": "http://10.57.16.172:11434"},
+    "nle": {"model": "llama3:70b-instruct-fp16", "api_key": "not-needed", "base_url": "http://10.57.16.172:11434"},
     "openai-gpt-3.5-turbo": {"model": "gpt-3.5-turbo-0125",  "api_key": os.environ["OPENAI_API_KEY"], "base_url": "https://api.openai.com/v1"},
     "openai-gpt-4": {"model": "gpt-4-0125-preview",  "api_key": os.environ["OPENAI_API_KEY"], "base_url": "https://api.openai.com/v1"},
-    "anthropic-haiku": {"model": "claude-3-haiku-20240307", "api_key": os.environ["ANTHROPIC_API_KEY"], "base_url": "https://api.anthropic.com"},
-    "anthropic-sonnet": {"model": "claude-3-sonnet-20240229", "api_key": os.environ["ANTHROPIC_API_KEY"], "base_url": "https://api.anthropic.com"}
+    "openai-gpt-4o": {"model": "gpt-4o-2024-05-13",  "api_key": os.environ["OPENAI_API_KEY"], "base_url": "https://api.openai.com/v1"}
+#    "anthropic-haiku": {"model": "claude-3-haiku-20240307", "api_key": os.environ["ANTHROPIC_API_KEY"], "base_url": "https://api.anthropic.com"},
+#    "anthropic-sonnet": {"model": "claude-3-sonnet-20240229", "api_key": os.environ["ANTHROPIC_API_KEY"], "base_url": "https://api.anthropic.com"}
 }
 
 DEFAULT = {
@@ -23,6 +29,13 @@ get_stamp = lambda c: datetime.now().strftime(f"%Y%m%d{c}%H%M%S")
 
 get_role = lambda m: {AIMessage: "assistant", HumanMessage: "human", SystemMessage: "system"}[type(m)]
 serialize_messages = lambda ms: [{"source": s, "role": get_role(m), "content": m.content} for (s, m) in ms]
+
+def load_meeting_name():
+    try:
+        with open('meeting_name.txt', 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        return ''
 
 def save():
     utterer = MODELS[st.session_state.utterer].copy()
@@ -49,8 +62,22 @@ def save():
 
     with open(f"save/{st.session_state.scenario.lower().replace(' ', '_')}.{user_id_stamp}.json", 'w') as json_file:
         json.dump(interaction, json_file)
-    
+
+def load_file(location, name, part):
+    filename = f"./{location}/{name}/{part}.md"
+    try:
+        with open(filename) as f:
+            system[part] = '\n'.join(f.readlines())
+    except FileNotFoundError:
+        system[part] = ""
+        st.error(f"File {filename} not found")
+    except Exception as e:
+        system[part] = ""
+        st.error(f"An error occurred: {e}")
+
+
 def upload(name="default"):
+
     location = "scenarios"
     ok = True
     try:
@@ -74,8 +101,12 @@ def upload(name="default"):
             if "filters" in scenario else ["be polite."]
         st.session_state.filter_emoji = scenario["filter_emoji"]\
             if "filter_emoji" in scenario else ":face_with_one_eyebrow_raised:"
+        st.session_state.principle_emoji = scenario["principle_emoji"]\
+            if "principle_emoji" in scenario else ":anxious_face_with_sweat:"
 
     system = dict()
+
+    # List of parts excluding 'meeting'
     parts = ["persona", "principles", "content"]
     for part in parts:
         try:
@@ -83,10 +114,51 @@ def upload(name="default"):
                 system[part] = '\n'.join(f.readlines())
         except:
                 system[part] = ""
+
+
+    # Add a file uploader for the user to upload the 'meeting' content dynamically
+    #the line below does not work so trying a different approach
+    #uploaded_file = st.file_uploader("Upload the meeting content file here:", type=["md"])
+    #ability to load a meeting file
+
+    if 'meeting_name' not in st.session_state:
+        st.session_state['meeting_name'] = load_meeting_name()
+
+
+    try:
+        with open(f"./{location}/{name}/{st.session_state['meeting_name']}.md") as f:
+            system["meeting"] = '\n'.join(f.readlines())
+            print("meeting file uploaded")
+    except:
+            system["meeting"] = ""
+            print("meeting file NOT uploaded")
+
+    # List of parts INCLUDING 'meeting'
+    parts = ["persona", "principles", "content", "meeting"]
+
     if "messages" not in st.session_state:
         st.session_state.messages = [
                 ("application", SystemMessage(content='\n'.join([system[p] for p in parts])))
             ]
+
+
+def principles(name="default"):
+    location = "scenarios"
+    ok = True
+    try:
+        scenario = json.loads(open (f"./{location}/{name}/config.json", "r").read())
+    except:
+        ok = False
+
+    part = "principles"
+    try:
+        with open(f"./{location}/{name}/{part}.md") as f:
+            princ = '\n'.join(f.readlines())
+    except:
+            princ = ""
+
+    return princ
+
 
 def connect(llm, params=DEFAULT, models=MODELS):
 
@@ -98,10 +170,15 @@ def connect(llm, params=DEFAULT, models=MODELS):
                 **{"anthropic_api_url": models[llm]["base_url"], "anthropic_api_key": models[llm]["api_key"]},
                 **parameters
             })
+    if llm.startswith("nle"):
+        client = ChatOllama(**{
+                **{"base_url": models[llm]["base_url"], "api_key": models[llm]["api_key"]},
+                **parameters
+            })
     else:
         client = ChatOpenAI(**{
                 **{"base_url": models[llm]["base_url"], "api_key": models[llm]["api_key"]},
                 **parameters
             })
-        
+
     return client
--- app/home.py
+++ app/home.py
@@ -2,15 +2,24 @@ from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
 import streamlit as st
+import re
 
-from app.common import get_role, connect, save, upload
+from app.common import get_role, connect, save, upload, principles
+
+
 
 if "scenario" not in st.session_state:
     params = st.query_params
+
     if "scenario" in params:
         upload(params["scenario"])
+        #myprinciples+=principles(params["scenario"])
+        st.session_state.myprinciples=principles(params["scenario"])
     else:
         upload("default")
+        #myprinciples+=principles("default")
+        st.session_state.myprinciples=principles("default")
+
 
 utterer = connect(st.session_state.utterer)
 filterer = connect(st.session_state.filterer, {"temperature": 0})
@@ -20,19 +29,19 @@ parser = StrOutputParser()
 help = """Note that the conversation is saved to a log file after each turn of utterance and response."""
 extract = lambda ms: [m for _, m in ms]
 
-def check(llm=utterer, parser=parser):
+def save_meeting_name():
+    with open('meeting_name.txt', 'w') as file:
+        file.write(st.session_state['meeting_name'])
 
-    system = """You only act as a filter in front of a specialized agent.
-Respond OK to the utterance if it respects the following guidelines, or politely explain that you cannot answer that request, with a short, to the point, justification, in English, if you decide to reject it.\n
-A valid utterance should:
-{}""".format("- " + "\n- ".join(st.session_state.filters))
 
+def check(llm=filterer, parser=parser):
+
+    system = """  """
     prompt = ChatPromptTemplate.from_messages([
         ("system", system),
          MessagesPlaceholder(variable_name="messages"),
     ])
     chain = prompt | llm | parser
-
     return chain.invoke({"messages": extract(st.session_state.messages[1::])})
 
 st.subheader(st.session_state.welcome)
@@ -42,22 +51,56 @@ for (source, message) in st.session_state.messages[1:]:
         st.markdown(((st.session_state.filter_emoji + " ") if "filterer" == source else "") + message.content)
 
 if utterance := st.chat_input("try me!"):
-    st.session_state.messages.append(("chat_input", HumanMessage(content=utterance)))
+
+    filterprompt = """You only act as a binary filter in front of a conversational agent. Your response to the utterance is OK if it respects the following guidelines and KO otherwise.
+    Guidelines: a valid (OK) utterance should:
+    {}""".format("- " + "\n- ".join(st.session_state.filters)+"\n Always add a short explanation after OK or KO about your decision. Now give your response for the following utterance: ")
+    utterance4filter=filterprompt+utterance
+    st.session_state.messages.append(("chat_input", HumanMessage(content=utterance4filter)))
     with st.chat_message("user"):
         st.markdown(utterance)
-
-    # all inputs are first checked by a filtering LLM
+    # all inputs are first checked by a filtering LLM, check() is applied to utterance4filter
     pre_check = check()
+    print("FILTER: ", pre_check)
+    print("\n")
     if pre_check.startswith("OK"):
         # answer from the main model, streamed
+        #need to go back from utterance4filter to utterance
+        st.session_state.messages.pop()
+        st.session_state.messages.append(("chat_input", HumanMessage(content=utterance)))
         with st.chat_message("assistant"):
             stream = utterer.stream(extract(st.session_state.messages))
             response = st.write_stream(stream)
-            st.session_state.messages.append(("utterer", AIMessage(content=response)))
+            #start check principles - remove all this to go back to previous version
+            #open principles.md filter_emoj
+            #end read principles.md file
+            principleprompt = """ You only act as a binary filter in front of a conversational agent. You should check if an utterance respect the following principles: \n"""
+            principleprompt+=st.session_state.myprinciples
+            response4principles=principleprompt+"Now answer OK if the following utterance respects the above principles and KO otherwise. Always add a short explanation about your decision: \n ["+response+"] "
+            #print("RESPONSE4PRINCIPLES: ", response4principles)
+            st.session_state.messages.append(("chat_input", HumanMessage(content=response4principles)))
+            pre_check = check()
+            print("PRINCIPLES: ", pre_check)
+            print("\n")
+            #end check principles
+            #do a pop here otherwise principles will be shown on the chat interface
+            if pre_check.startswith("OK"): #in PRINCIPLES=OK
+                st.session_state.messages.pop()
+                st.session_state.messages.append(("utterer", AIMessage(content=response)))
+            else: #PRINCIPLES=KO
+                st.session_state.messages.pop()
+                st.markdown(st.session_state.principle_emoji + " [This answer might not respect my principles!]\n " )
+                #st.session_state.messages.append(("utterer", AIMessage(content=pre_check)))
+                st.session_state.messages.append(("utterer", AIMessage(content=response)))
     else:
         # answer from the filtering model
+        #this pop is absolutaly needed here otherwise filter info will pollute the conversation history
+        st.session_state.messages.pop()
         with st.chat_message("assistant"):
-            st.markdown(st.session_state.filter_emoji + " " + pre_check)
+            tmp=re.sub("KO", "", pre_check)
+            explanation=re.sub("Explanation:", "", tmp)
+            #st.markdown(st.session_state.filter_emoji + " " + pre_check)
+            st.markdown(st.session_state.filter_emoji + " " + explanation)
         st.session_state.messages.append(("filterer", AIMessage(content=pre_check)))
 
 save()
@@ -73,3 +116,8 @@ with st.sidebar:
 
     st.subheader(":information_source: if you need some help")
     st.markdown(help)
+    st.divider()
+
+    st.subheader(":green_book:  meeting transcript file")
+    st.session_state['meeting_name'] = st.text_input("Enter the meeting name:")
+    save_meeting_name()
